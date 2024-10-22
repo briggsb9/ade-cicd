b484943fa98bae93cb760f1a7313ce5026b4e956
briggsb9 <ben_briggs@outlook.com>
2024-10-22 18:19:49 +0100

    Refactor ARM templates and update entry point in Program.cs

--- ADE.Tutorial/Program.cs
+++ ADE.Tutorial/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.Extensions.Hosting;
-
+// Program.cs is the entry point for the Azure Functions worker runtime.
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .Build();
