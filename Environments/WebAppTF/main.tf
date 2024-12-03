0b38f7b94e7f9a457a25279c550ea58516b78c86
briggsb9 <ben_briggs@outlook.com>
2024-12-03 18:40:45 +0000

    terra tests

--- Environments/WebAppTF/main.tf
+++ Environments/WebAppTF/main.tf
@@ -1,7 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "name" {
+  description = "The name of the resources"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resources"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+
 resource "azurerm_app_service_plan" "example" {
     name                = "${var.name}-asp"
-    location            = ade_environment_location
-    resource_group_name = ade_resource_group_name
+    location            = location
+    resource_group_name = resource_group_name
     sku {
         tier = "Standard"
         size = "S1"
@@ -10,8 +30,8 @@ resource "azurerm_app_service_plan" "example" {
 
 resource "azurerm_app_service" "example" {
     name                = "${var.name}-app"
-    location            = ade_environment_location
-    resource_group_name = ade_resource_group_name
+    location            = location
+    resource_group_name = resource_group_name
     app_service_plan_id = azurerm_app_service_plan.example.id
 
     site_config {
@@ -21,4 +41,14 @@ resource "azurerm_app_service" "example" {
     app_settings = {
         "WEBSITE_RUN_FROM_PACKAGE" = "1"
     }
+}
+
+output "app_service_name" {
+  description = "The name of the App Service"
+  value       = azurerm_app_service.example.name
+}
+
+output "app_service_plan_name" {
+  description = "The name of the App Service Plan"
+  value       = azurerm_app_service_plan.example.name
 }
--- Environments/WebAppTF/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "app_service_name" {
-  description = "The name of the App Service"
-  value       = azurerm_app_service.example.name
-}
-
-output "app_service_plan_name" {
-  description = "The name of the App Service Plan"
-  value       = azurerm_app_service_plan.example.name
-}
